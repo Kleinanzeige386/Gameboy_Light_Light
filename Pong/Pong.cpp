05b8e67eaf2b8a11c32302ee4eb39350b83b3bb5
Kleinanzeige386 <95414855+Kleinanzeige386@users.noreply.github.com>
2022-10-19 12:29:16 +0200

    Refined Pong.cpp code
    Made the code more readable, organized and optimized

--- Pong/Pong.cpp
+++ Pong/Pong.cpp
@@ -1,257 +1,281 @@
+/*
+ * Includes
+ */
 #include <stdio.h>
 #include<windows.h> //remove
-#include <conio.h>
+#include <conio.h> //remove
+
+#pragma clang diagnostic pop// remove
 
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Writable-strings"
+/*
+ * Constants
+ */
+const int MATRIXWIDTH = 16;     //Number of collumns in the matrix
+const int MATRIXHEIGHT = 8;     //Number of rows in the matrix
+const int POINTSTOWIN = 100;    //Number of Points, needed to win
+const int MSperStep=50;        // Miliseconds between steps
 
-int MATRIXWIDTH = 16;
-int MATRIXHEIGHT = 8;
-int POINTSTOWIN = 100;
-int MSperStep=1;
-int game=0;
-
-int ball[2];
-int ballvelocity[2];
-
-int player;
-int bot;
-int playervelocity; //replace wit poti
-int botvelocity;
-int playerpoints[2];
-
-
-
-void playround();
-void resetround();
-void simulate();
-int checkWin();
-
-bool isXBorder(int n);
-bool isYBorder(int n);
-bool hitsPlayers();
-void checkVelo();
-void changeBotVeloSmart();
-void movePlayers();
-void moveBall();
 
 /*
-Grid:
-    00  10  20  30
-    01  11  21  31
-    02  12  22  32
-    04  13  23  33
-*/
+ * Datatypes
+ */
+enum Border{LEFT_BORDER = 0, RIGHT_BORDER = MATRIXWIDTH-1, UPPER_BORDER = 0, LOWER_BORDER = MATRIXHEIGHT-1, };
+enum Direction{LEFT = -1, RIGHT = 1,UP = -1,DOWN = 1};
 
-void playround(){
+typedef struct Player{
+    int yCoordinate;
+    Border border;
+    Direction direction;
+}Player;
 
-    printf("\n%d : %d",playerpoints[0],playerpoints[1]); //REMOVE
+
+/*
+ *  Global variables
+ */
+int game=0; //Number of Games played
+int playerPoints[2];    //Points achieved by   User | Bot
+
+int ball[2];    // x | y coordinates of the ball
+Direction ballVelocity[2];    // x | y directions of the ball
+
+Player user;    // Human player
+Player bot;     // Bot player
+
+
+/*
+ * Grid schema
+ *
+ *
+ *Grid:
+ *   00  10  20  30 .. n0
+ *   01  11  21  31 .. n1
+ *   02  12  22  32 .. n2
+ *   04  13  23  33 .. n3
+ *   ..  ..  ..  ..    ..
+ *   0n  1n  2n  3n .. nn
+ */
+
+
+/*
+ * Function prototypes
+ */
+int main(int argc, char const *argv[]); //Main program
+
+void playRound();   //plays a round of Pong until one Player scores a point
+void resetRound();  //resets all needed variables
+bool checkWin();     //checks whether the ball hits a goal
+
+void moveBall();//moves the ball 1 space in each of its directions
+int hitsPlayer();
+void checkBallVelo();   //checks whether the ball hits a border / Player
+
+void changeBotVeloSmart();  //Used by bot player to track the ball movement
+void movePlayers();    //moves players
+
+void simulate();    //Prints the game to the console, !!!!!remove when porting!!!!!
+
+
+/*
+ * Functions
+ */
+
+int main(int argc, char const *argv[])
+{
+    user.border = LEFT_BORDER;
+    bot.border = RIGHT_BORDER;
+
+    while((playerPoints[0]<=POINTSTOWIN) && (playerPoints[1]<=POINTSTOWIN)){
+        playRound();
+        game +=1;
+    }
+    //system("cls");   //remove
+    if (playerPoints[0]>playerPoints[1]) printf("player 1 Won!");  else printf("player 2 Won!");
+
+
+
+
+    scanf(("\nEnd\n")); //remove
+
+
+
+    return 0;
+}
+
+void playRound(){
+    resetRound();
+
+    printf("\n%d : %d",playerPoints[0],playerPoints[1]); //REMOVE
     simulate(); //Remove
     int iterations=0;//Remove
     int outOfBounds=0;//Remove
 
-    resetround();
-    while(checkWin()==0){
-
-
-        //Sleep(MSperStep); // Replace with delay(x)
-       
-        movePlayers(); 
-        checkVelo();
-        
+    while(!checkWin()){
+        Sleep(MSperStep); // Replace with delay(x)
+        movePlayers();
         moveBall();
 
-       system("cls");   //remove
-       simulate(); //Remove
-        
+        simulate(); //Remove
     }
 
 }
 
-void resetround(){
+void resetRound(){
     ball[0]=8;
     ball[1]=4;
 
-    ballvelocity[0]=1;
-    ballvelocity[1]=1;
+    ballVelocity[0]=DOWN;
+    ballVelocity[1]=RIGHT;
 
-    player=0;
-    bot=0;
-    playervelocity=-1; //replace wit poti
-    botvelocity=-1;
+    user.yCoordinate = 0;
+    user.direction = DOWN;
 
+    bot.yCoordinate = 0;
+    bot.direction = DOWN;
+}
+
+bool checkWin(){    //Is Ball on the far left or right side of the Matrix?
+
+    if (ball[0] == (RIGHT_BORDER))
+    {
+        playerPoints[0]++;
+        return true;   //Left Player wins
+    }else if (ball[0] == LEFT_BORDER)   //Left Side
+    {
+        playerPoints[1]++;
+        return true; //Right Player wins
+    }
+
+    return false;
+
+}
+
+
+void moveBall(){
+    checkBallVelo();
+    ball[0]+=ballVelocity[0];
+    ball[1]+=ballVelocity[1];
+}
+
+void checkBallVelo(){
+    if( ( ball[0]==LEFT_BORDER ) || ( hitsPlayer()== -1) )//Ball hits leftBorder or User
+    {
+        ballVelocity[0] = RIGHT;
+    }
+
+    if( ( ball[0]==RIGHT_BORDER ) || ( hitsPlayer()== 1) )//Ball hits leftBorder or User
+    {
+        ballVelocity[0] = LEFT;
+    }
+
+
+    if( ball[1] == UPPER_BORDER)//Ball hits UpperBorder
+    {
+        ballVelocity[1] = DOWN;
+    }
+
+    if( ball[1] == LOWER_BORDER)//Ball hits LowerBorder
+    {
+        ballVelocity[1] = UP;
+    }
 
 
 }
+
+int hitsPlayer(){
+    if( ball[0]==LEFT_BORDER+1 ){ //does ball hit user?
+        if( (ball[1]== user.yCoordinate) || (ball[1]== user.yCoordinate+1)){
+            return -1;
+        }
+    }
+
+    if( ball[0]==RIGHT_BORDER-1 ){ //does ball hit bot?
+        if(( ball[1]== bot.yCoordinate) || (ball[1]== bot.yCoordinate+1)){
+            return 1;
+        }
+    }
+
+    return 0; //ball doesnt hit anything
+}// -1 == user | 1 == bot | 0==false
+
+
+Player changeBotVeloSmart(Player player){
+
+    if(player.yCoordinate+1 > ball[1]){ //Is players head lower than ball?
+        player.direction =UP;                                                                                                             //              bot
+    }                                                                                                                                           //              bot+1
+    if ((player.yCoordinate)<ball[1]){ //Is players body higher than ball?
+        player.direction = DOWN;
+    }
+
+
+    switch (player.yCoordinate)    //Make sure Player doesnt collide with border
+    {
+        case UPPER_BORDER: //Head hits upper Border
+            player.direction = DOWN;
+            break;
+
+        case LOWER_BORDER: //Body hits lower Border
+            player.direction =UP;
+            break;
+
+        default:
+            break;
+    }
+
+    return player;
+}
+
+void movePlayers(){
+    user = changeBotVeloSmart(bot);
+    bot = changeBotVeloSmart(user);
+
+    user.yCoordinate += user.direction;
+    bot.yCoordinate  += bot.direction;
+}
+
 
 void simulate(){ //Remove
     /*
     * SIMULATION START
     */
+    system("cls");
+    printf("\nGame: %d\n\n Ball:  %d|%d\n Player:  %d\n Bot: %d\n\n",game,ball[0],ball[1],playerPoints[0],playerPoints[1]);
 
-   printf("\nGame: %d\n\n Ball:  %d|%d\n Player:  %d\n Bot: %d\n\n",game,ball[0],ball[1],playerpoints[0],playerpoints[1]);
 
-   /*
     for (int i = 0; i < MATRIXHEIGHT ; i++) //Go through Matrix rows
     {
         printf("\n"); //Start new row
-        
+
         for (int  j = 0; j < 16 ; j++)//Go through Matrix Collumns
         {
             char* temp="- ";
-          
+
             if ((ball[0]==j) && (ball[1]==i)) //Is Ball in that position?
             {
                 temp="O ";
-            }else if (      j==0 && ((player==i)  ||  player==i-1))
+            }else if (      j==0 && ((user.yCoordinate ==i)  ||  user.yCoordinate==i-1))
             {
                 temp="P ";
-            }else if (    j==(MATRIXWIDTH-1) && ((bot==i)  ||  bot==i-1))
+            }else if (    j==(MATRIXWIDTH-1) && ((bot.yCoordinate ==i)  ||  bot.yCoordinate ==i-1))
             {
-                temp="B "; 
+                temp="B ";
             }
 
-            printf(temp);
-            
+            printf("%s", temp);
+
         }
-        
+
     }
     printf("\n\n");
-    */
-    
-   /*
-    * SIMULATION END
-    */
-}
 
-int checkWin(){    //Is Ball on the far left or right side of the Matrix?
 
-    if (ball[0]==(MATRIXWIDTH-1)) //Right Side
-    {
-        playerpoints[0]++;
-        return 1;   //Left Player wins
-    }else if (ball[0]==0)   //Left Side
-    {
-        playerpoints[1]++;
-        return 2; //Right Player wins
-    }
-
-    return 0;
-    
+    /*
+     * SIMULATION END
+     */
 }
 
 
-bool isXBorder(int n){
-    if (n>=(MATRIXWIDTH-1) || n<=0) return true;
-    return false;
-}
 
-bool isYBorder(int n){
-    if (n>=(MATRIXHEIGHT-1) || n<=0) return true;
-    return false;
-}
-
-bool hitsPlayers(){
-
-    switch (ball[0]) //x-Position Ball
-    {
-    case 1:
-        if( (ball[1]==player)   ||  (ball[1]==player+1)   ){
-            return true;
-        }
-        break;
-
-    case 14:
-        if( (ball[1]==bot)   ||  (ball[1]==bot+1)   ){
-            return true;
-        }
-        break;    
-    
-    default:
-        return false;
-        break;
-    }
-
-    return false;
-}
-
-void checkVelo(){
-    if (isYBorder(ball[1]))  //Top or Bottom
-    {
-        ballvelocity[1] *= -1;
-    }
-    if (isXBorder(ball[0]) || hitsPlayers())   //Right or Left
-    {
-         ballvelocity[0] *= -1;
-    }
-
-    
-
-    
-}
-
-void movePlayers(){
-    changeBotVeloSmart();
-    if (isYBorder(player) || isYBorder(player+1))
-    {
-        playervelocity *= -1;
-    }
-
-
-    player += playervelocity;
-    bot += botvelocity;
-    
-}
-
-void moveBall(){
-    ball[0]+=ballvelocity[0];
-    ball[1]+=ballvelocity[1];
-}
-
-void changeBotVeloSmart(){
-    
-    if(bot>ball[1]){    
-        botvelocity=1;                                                                                                             //              bot
-    }                                                                                                                                           //              bot+1
-    if ((bot+1)>ball[1])
-    {
-        botvelocity=-1;
-    }
-
-
-    switch (bot)
-    {
-    case 0:
-            botvelocity=1;
-        break;
-
-    case 6:
-            botvelocity=-1;
-        break;    
-    
-    default:
-        break;
-    }
-
-    
-}
-
-
-int main(int argc, char const *argv[])
-{
-
-    while((playerpoints[0]<=POINTSTOWIN) && (playerpoints[1]<=POINTSTOWIN)){
-        playround();
-        game +=1;
-    }
-    //system("cls");   //remove
-    if (playerpoints[0]>playerpoints[1]) printf("player 1 Won!");  else printf("player 2 Won!");
-    
-
-
-
-    scanf(("\nEnd\n"));
-
-    
-
-    return 0;
-}
